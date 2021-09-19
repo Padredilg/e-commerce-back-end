bd7d8f2c918dfeb4459910ada3830fab9853f7ef
Padredilg <padredilg@knights.ucf.edu>
2021-09-19 19:24:34 -0400

    added associations to models/index

--- models/index.js
+++ models/index.js
@@ -4,12 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  //as: 'product_tag',
+  foreignKey: 'product_id'
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  //as: 'product_tag',
+  foreignKey: 'tag_id'
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
