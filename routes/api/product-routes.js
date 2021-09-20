10c781c1369896c9059a0b02c08bad97360ad37e
Padredilg <padredilg@knights.ucf.edu>
2021-09-20 06:06:03 -0400

    writing the product GET all route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,8 +3,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// GET all products
 router.get('/', (req, res) => {
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
   // find all products
   // be sure to include its associated Category and Tag data
 });
